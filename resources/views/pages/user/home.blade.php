4ad9ab8aa15b427d78592ea02f0ca065601a52a6
Nova Sriady <andiarkurnia1234@gmail.com>
2024-06-06 16:54:26 +0700

    upp

--- resources/views/pages/user/home.blade.php
+++ resources/views/pages/user/home.blade.php
@@ -18,20 +18,20 @@
     <section class="mt-20 w-full px-12" id="productSection">
         <div class="flex flex-col gap-8">
             <div class="flex flex-col gap-2">
-                <h1 class="font-semibold text-2xl">Produk Kami</h1>
-                <p>Pilih produk yang Anda inginkan sekarang!</p>
+                <h1 class="font-semibold text-2xl text-center">Produk Pilihan</h1>
+                <p class="text-center">Pilih produk yang Anda inginkan sekarang!</p>
             </div>
             <div class="grid grid-cols-4 gap-x-6 gap-y-8">
                 @foreach ($products as $product)
-                    <div class="card card-compact w-full bg-white rounded-xl shadow-md">
+                    <div class="card card-compact w-full bg-slate-400 rounded-xl shadow-md">
                         <figure class="h-56 overflow-hidden">
                             <img src="{{ asset('storage/' . $product->product_img) }}" alt="Product Image" class="w-full" />
                         </figure>
                         <div class="card-body">
                             <h2 class="card-title">{{ $product->product_name }}</h2>
-                            <p>{{ $product->product_description }}</p>
+                            <h2 class="card-title text-red-600 text-bold">Rp.{{ $product->product_price }}</h2>
                             <div class="card-actions justify-start mt-2">
-                                <a href="{{ route('user.productDetails', ['product_id' => $product->product_id]) }}" class="btn bg-gray-800 text-white btn-sm">Cek Sekarang</a>
+                                <a href="{{ route('user.productDetails', ['product_id' => $product->product_id]) }}" class="btn bg-gray-800 text-white btn-sm">Pesan</a>
                             </div>
                         </div>
                     </div>
@@ -43,8 +43,8 @@
     <section class="mt-20 w-full px-12">
         <div class="flex flex-col gap-8">
             <div class="flex flex-col gap-2">
-                <h1 class="font-semibold text-2xl">Kategori Produk</h1>
-                <p>Berbagai macam kategori produk untuk dipilih.</p>
+                <h1 class="font-semibold text-2xl text-center">Kategori Produk</h1>
+                <p class="text-center">Berbagai macam kategori produk untuk dipilih.</p>
             </div>
             <div class="grid grid-cols-4 gap-x-6 gap-y-8">
                 @foreach ($categories as $category)
